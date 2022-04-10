0d499ea934cd0227c1debb0024cbc9c17d2275eb
cristiano <roopampatil10@gmail.com>
2022-04-10 15:07:14 +0530

    git init commit

---
+++ src/main/java/com/iiitb/roopam/RoopamApplication.java
@@ -0,0 +1,13 @@
+package com.iiitb.roopam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoopamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoopamApplication.class, args);
+    }
+
+}
